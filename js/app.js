e4db3af05ce51f7d2e5cedf231b723ff1c67ad3b
ninjahatodi1 <45872727+ninjahatodi1@users.noreply.github.com>
2019-10-24 21:27:06 +0530

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,35 @@
+const copy = document.getElementById('copy');
+copy.addEventListener('click', function () {
+  copy.innerText = "copied to Clipboard";
+  setTimeout(() => {
+    copy.innerHTML = ` Get the link
+      <span> <i class="far fa-clipboard"></i></span>`;
+  }, 1000);
+});
+
+function copyToClipboard(element) {
+  var $temp = $("<input>");
+  $("body").append($temp);
+  $temp.val($(element).text()).select();
+  document.execCommand("copy");
+  $temp.remove();
+}
+
+document.addEventListener('keydown', function (event) {
+  if (event.which == 123)
+    document.querySelector('#openModal').click();
+  else if (event.ctrlKey && event.shiftKey && event.keyCode == 73)
+    document.querySelector('#openModal').click();
+});
+
+
+document.oncontextmenu = function () {
+  return false;
+}
+
+document.onkeydown = function (event) {
+  if (event.which == 123)
+    return false;
+  else if (event.ctrlKey && event.shiftKey && event.keyCode == 73)
+    return false;
+}
